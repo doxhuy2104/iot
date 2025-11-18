45759c1aa08962d65cc4c680b4f015f1f993ee9d
Trần Văn Khánh <khanhlvt18@gmail.com>
2025-11-18 21:35:45 +0700

    init khanh-dev

---
+++ smart-watering-backend/src/test/java/com/iot/smartwatering/smart_watering_backend/SmartWateringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iot.smartwatering.smart_watering_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartWateringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
